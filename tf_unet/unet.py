b649220493c1ab6bc7c0d0afb16180e082cc86ae
ameya005 <ameya@sigtuple.com>
2017-03-22 18:25:43 +0530

    Added gpu memory throttle option

--- tf_unet/unet.py
+++ tf_unet/unet.py
@@ -251,9 +251,9 @@ class Unet(object):
         """
         
         init = tf.global_variables_initializer()
-        gpu_options = tf.GPUOptions(allow_growth=True)
-        
-        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.5
+        with tf.Session(config=config) as sess:
             # Initialize variables
             sess.run(init)
         
@@ -271,8 +271,9 @@ class Unet(object):
         """
 
         init = tf.global_variables_initializer()
-        gpu_options = tf.GPUOptions(allow_growth=True)
-        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.5
+        with tf.Session(config=config) as sess:
             #init variables
             sess.run(init)
 
@@ -428,8 +429,9 @@ class Trainer(object):
         init = self._initialize(training_iters, output_path, restore)
         saver = tf.train.Saver()
        
-        gpu_options = tf.GPUOptions(allow_growth=True)
-        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.5
+        with tf.Session(config=config) as sess:
             sess.run(init)
             
             if restore:
